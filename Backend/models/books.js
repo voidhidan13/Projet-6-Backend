d913356ceb2e1ca648de27db8f5e59e227d9f62c
MaevaG <maeva.garcia13210@gmail.com>
2024-09-10 13:24:21 +0200

    ajout du ratings et genre sur le schema books

--- Backend/models/books.js
+++ Backend/models/books.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
 
-const BooksSchema = mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    imageUrl: { type: String, required: true },
-    userId: { type: String, required: true },
-    price: { type: Number, required: true },
+const booksSchema = mongoose.Schema({
+
+userId: { type: String, required: true },
+title: { type: String, required: true },
+author: { type: String, required: true },
+imageUrl: { type: String, required: true },
+year: { type: Number, required: true },
+genre: { type: String, required: true },
+
+ratings: [
+    {
+      userId: { type: String, required: true },
+      grade: { type: Number, required: true }, 
+    },
+  ],
+  averageRating: { type: Number, required: true }, 
 });
 
-module.exports = mongoose.model('Books', BooksSchema);
+module.exports = mongoose.model('Books', BooksSchema); 
