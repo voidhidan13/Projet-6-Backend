8fec5c48e99f06e9505d2237bcb05656f15b3e94
MaevaG <maeva.garcia13210@gmail.com>
2024-09-30 15:35:05 +0200

    Ajout de cors et parsing dans app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,13 @@
 require('dotenv').config();  // Charger les variables d'environnement
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require("path"); // Importer path
+const cors = require('cors'); // Ajout de CORS directement
 const app = express();
-
-
-// Import middleware
-const parsingMiddleware = require('./middleware/parsing');  
-const corsMiddleware = require('./middleware/cors');  
 
 // Import routes
 const booksRoutes = require('./routes/books'); 
 const usersRoutes = require('./routes/users');
-
-// Importer le fichier de routes des livres
 
 console.log('MONGO_URI:', process.env.MONGO_URI);
 
@@ -21,20 +16,23 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connexion réussie à MongoDB'))
   .catch(error => console.error('Erreur de connexion à MongoDB :', error));
 
-// Utiliser les middlewares
-app.use(parsingMiddleware);  
-app.use(corsMiddleware);  
+// Utiliser CORS directement
+app.use(cors()); // Applique CORS sur toutes les routes
+
+// Utiliser le middleware pour parser le JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Pour les données encodées en URL, si nécessaire
+
+// Servir les fichiers statiques du dossier images
+app.use('/images', express.static(path.join(__dirname, 'images'))); // Serve les fichiers du dossier images
 
 // Route pour la racine
 app.get('/', (req, res) => {
   res.send('Bienvenue sur la page d\'accueil !');
 });
 
-
-// Utiliser les routes pour les livres, avec le chemin de base '/api/books'
+// Utiliser les routes pour les livres et les utilisateurs
 app.use('/api/books', booksRoutes);
 app.use('/api/auth', usersRoutes);
-
-
 
 module.exports = app;
--- Backend/middleware/parsing.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const parsingMiddleware = express();
-
-// Middleware pour parser le JSON
-parsingMiddleware.use(express.json());
-
-module.exports = parsingMiddleware;
